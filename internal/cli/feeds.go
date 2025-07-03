9375a671a1489de718a39c5b3cd96817a26339d7
kavancamp <keenahvancampenhout@gmail.com>
2025-07-03 18:47:19 -0500

    added feeds handler

---
+++ internal/cli/feeds.go
@@ -0,0 +1,27 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	//"github.com/kavancamp/blogAggregator/internal/feeds"
+)
+
+func init() {
+	RegisterCommand("feeds", feedsHandler)
+}
+func feedsHandler(state *State, cmd Command) error {
+	if len(cmd.Args) != 0 {
+		return fmt.Errorf("usage: gator feeds")
+	}
+
+	feeds, err := state.DB.ListFeeds(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to list feeds: %w", err)
+	}
+
+	for _, feed := range feeds {
+		fmt.Printf("Feed: %s\nURL: %s\nAdded by: %s\n\n", feed.FeedName, feed.FeedUrl, feed.UserName)
+	}
+
+	return nil
+}
