37a84545a742e8fd367860eb45eda3aee897e121
kavancamp <keenahvancampenhout@gmail.com>
2025-07-03 18:47:04 -0500

    added handler for feeds cmd

---
+++ internal/cli/addfeed.go
@@ -0,0 +1,50 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/kavancamp/blogAggregator/internal/database"
+)
+
+func init() {
+	RegisterCommand("addfeed", addFeedHandler)
+}
+
+func addFeedHandler(state *State, cmd Command) error {
+	if len(cmd.Args) != 2 {
+		return fmt.Errorf("usage: gator addfeed <name> <url>")
+	}
+
+	currentUserName := state.Config.CurrentUserName
+	if currentUserName == "unknown" {
+		return fmt.Errorf("no user currently logged in")
+	}
+
+	user, err := state.DB.GetUser(context.Background(), currentUserName)
+	if err != nil {
+		return fmt.Errorf("failed to get user: %w", err)
+	}
+
+	id := uuid.New()
+	now := time.Now()
+
+	feed, err := state.DB.CreateFeed(context.Background(), 
+		database.CreateFeedParams{
+			ID:        id,
+			CreatedAt: now,
+			UpdatedAt: now,
+			Name:      cmd.Args[0],
+			Url:       cmd.Args[1],
+			UserID:    user.ID,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create feed: %w", err)
+	}
+
+	fmt.Printf("Feed created:\nID: %s\nName: %s\nURL: %s\nUserID: %s\n", feed.ID, feed.Name, feed.Url, feed.UserID)
+	return nil
+}
