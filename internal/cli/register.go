67ed1c4eae0626bb1662b576b03e35fa2eee5ea5
kavancamp <keenahvancampenhout@gmail.com>
2025-07-03 16:53:35 -0500

    added registration

---
+++ internal/cli/register.go
@@ -0,0 +1,49 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"database/sql"
+	"time"
+		"github.com/google/uuid"
+	"github.com/kavancamp/blogAggregator/internal/database"
+)
+
+func init() {
+	RegisterCommand("register", registerHandler)
+}
+
+func registerHandler(state *State, cmd Command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: gator register <username>")
+	}
+	userName := cmd.Args[0]
+
+	_, err := state.DB.GetUser(context.Background(), userName)
+	if err == nil {
+		return fmt.Errorf("user %s already exists", userName)
+	}
+	if err != sql.ErrNoRows {
+		return fmt.Errorf("error checking for user: %w", err)
+	}
+
+	newUser := database.CreateUserParams{
+		ID: uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name: userName,
+	}
+
+	user, err := state.DB.CreateUser(context.Background(), newUser)
+	if err != nil {
+		fmt.Printf("CreateUser failed: %v", err)
+		return fmt.Errorf("user %s already exists", userName)
+	}
+	//update config file with new user
+	if err := state.Config.SetUser(user.Name); err != nil {
+		return fmt.Errorf("failed to set user in config: %w", err)
+	}
+	fmt.Printf("User created: %+v\n", user)
+	return nil
+
+}
