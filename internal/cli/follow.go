9f33ae279b691882061a2ba623f58296721fc236
kavancamp <keenahvancampenhout@gmail.com>
2025-07-03 19:47:38 -0500

    added follow command

---
+++ internal/cli/follow.go
@@ -0,0 +1,48 @@
+package cli
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/kavancamp/blogAggregator/internal/database"
+)
+
+func init() {
+	RegisterCommand("follow", followHandler)
+}
+
+func followHandler(state *State, cmd Command) error {
+	//It takes a single url argument and creates a new feed follow record for the current user. 
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: gator follow <feed_url>")
+	}
+	feedURL := cmd.Args[0]
+	user, err := getCurrentUser(state)
+	if err != nil {
+		return err
+	}
+	
+	feed, err := state.DB.GetFeedByURL(context.Background(), feedURL)
+	if err != nil {
+		return fmt.Errorf("could not find feed with URL %s: %w", feedURL, err)
+	}
+
+	now := time.Now()
+	params := database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	}
+
+	followRecord, err := state.DB.CreateFeedFollow(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("could not follow feed: %w", err)
+	}
+	//print the name of the feed and the current user once the record is created 
+	fmt.Printf("Now following '%s' as user '%s'\n", followRecord.FeedName, followRecord.UserName)
+	return nil
+}
